d0e68f3f21cf605ff6adf7453254e8219bc8a621
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2019-10-07 11:37:19 +0800

    esp-wolfssl: add updated libs and documentation
    - CMake/Make: Added support for WOLFSSL_SMALL_CERT_VERIFY and Debug
    - Added newly compiled libraries downloaded from esp-wolfssl build

--- component.mk
+++ component.mk
@@ -1,7 +1,26 @@
 COMPONENT_ADD_INCLUDEDIRS := port wolfssl
 
-COMPONENT_ADD_LDFLAGS := $(COMPONENT_PATH)/lib/libwolfssl.a
-
-COMPONENT_ADD_LINKER_DEPS := $(COMPONENT_PATH)/lib/libwolfssl.a
+ifdef CONFIG_ESP_DEBUG_WOLFSSL
+    ifdef CONFIG_ESP_WOLFSSL_SMALL_CERT_VERIFY
+        # use debug library with WOLFSSL_SMALL_CERT_VERIFY enabled
+        COMPONENT_ADD_LDFLAGS := $(COMPONENT_PATH)/lib/libwolfssl_small_cert_verify_debug.a
+        COMPONENT_ADD_LINKER_DEPS := $(COMPONENT_PATH)/lib/libwolfssl_small_cert_verify_debug.a
+    else
+        # use debug library without WOLFSSL_SMALL_CERT_VERIFY enabled
+        COMPONENT_ADD_LDFLAGS := $(COMPONENT_PATH)/lib/libwolfssl_debug.a
+        COMPONENT_ADD_LINKER_DEPS := $(COMPONENT_PATH)/lib/libwolfssl_debug.a
+    endif
+else
+    ifdef CONFIG_ESP_WOLFSSL_SMALL_CERT_VERIFY
+        # use library with WOLFSSL_SMALL_CERT_VERIFY enabled (without debug)
+        COMPONENT_ADD_LDFLAGS := $(COMPONENT_PATH)/lib/libwolfssl_small_cert_verify.a
+        COMPONENT_ADD_LINKER_DEPS := $(COMPONENT_PATH)/lib/libwolfssl_small_cert_verify.a
+    else
+        # use library without WOLFSSL_SMALL_CERT_VERIFY enabled (without debug)
+        # This Library is USED BY DEFAULT (no wolfssl options enabled)
+        COMPONENT_ADD_LDFLAGS := $(COMPONENT_PATH)/lib/libwolfssl.a
+        COMPONENT_ADD_LINKER_DEPS := $(COMPONENT_PATH)/lib/libwolfssl.a
+    endif
+endif
 
 CFLAGS +=-DWOLFSSL_USER_SETTINGS
