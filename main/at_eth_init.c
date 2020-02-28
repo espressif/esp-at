a577c7967bc92820ca34fe0f7df3760b75d08f4b
Zhao Ke Ke <zhaokeke@espressif.com>
2020-02-28 09:40:24 +0800

    fix: modift ETH to used on latest AT version

--- main/at_eth_init.c
+++ main/at_eth_init.c
@@ -24,86 +24,31 @@
 #include "tcpip_adapter.h"
 
 #ifdef CONFIG_AT_ETHERNET_SUPPORT
-
-#ifdef CONFIG_PHY_LAN8720
-#include "eth_phy/phy_lan8720.h"
-#define DEFAULT_ETHERNET_PHY_CONFIG phy_lan8720_default_ethernet_config
-#endif
-#ifdef CONFIG_PHY_TLK110
-#include "eth_phy/phy_tlk110.h"
-#define DEFAULT_ETHERNET_PHY_CONFIG phy_tlk110_default_ethernet_config
-#endif
-
-#define PIN_PHY_POWER CONFIG_PHY_POWER_PIN
-#define PIN_SMI_MDC   CONFIG_PHY_SMI_MDC_PIN
-#define PIN_SMI_MDIO  CONFIG_PHY_SMI_MDIO_PIN
-
-#ifdef CONFIG_PHY_USE_POWER_PIN
-/* This replaces the default PHY power on/off function with one that
-   also uses a GPIO for power on/off.
-
-   If this GPIO is not connected on your device (and PHY is always powered), you can use the default PHY-specific power
-   on/off function rather than overriding with this one.
-*/
-static void phy_device_power_enable_via_gpio(bool enable)
-{
-    assert(DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable);
-
-    if (!enable) {
-        /* Do the PHY-specific power_enable(false) function before powering down */
-        DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable(false);
-    }
-
-    gpio_pad_select_gpio(PIN_PHY_POWER);
-    gpio_set_direction(PIN_PHY_POWER,GPIO_MODE_OUTPUT);
-    if(enable == true) {
-        gpio_set_level(PIN_PHY_POWER, 1);
-        printf("phy_device_power_enable(TRUE)");
-    } else {
-        gpio_set_level(PIN_PHY_POWER, 0);
-        printf("power_enable(FALSE)");
-    }
-
-    // Allow the power up/down to take effect, min 300us
-    vTaskDelay(1);
-
-    if (enable) {
-        /* Run the PHY-specific power on operations now the PHY has power */
-        DEFAULT_ETHERNET_PHY_CONFIG.phy_power_enable(true);
-    }
-}
-#endif
-
-static void eth_gpio_config_rmii(void)
-{
-    phy_rmii_configure_data_interface_pins();
-    phy_rmii_smi_configure_pins(PIN_SMI_MDC, PIN_SMI_MDIO);
-}
+#include "esp_eth.h"
 
 bool at_eth_init(void)
 {
     esp_err_t ret = ESP_OK;
-    eth_config_t config = DEFAULT_ETHERNET_PHY_CONFIG;
-    /* Set the PHY address in the configuration */
-    config.phy_addr = CONFIG_PHY_ADDRESS;
-    config.gpio_config = eth_gpio_config_rmii;
-    config.tcpip_input = tcpip_adapter_eth_input;
-    config.clock_mode = CONFIG_PHY_CLOCK_MODE;
+    ESP_ERROR_CHECK(tcpip_adapter_set_default_eth_handlers());
 
-#ifdef CONFIG_PHY_USE_POWER_PIN
-    /* Replace the default 'power enable' function with an specific
-       one that toggles a power GPIO. */
-    config.phy_power_enable = phy_device_power_enable_via_gpio;
+    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&mac_config);
+#if CONFIG_PHY_IP101
+    esp_eth_phy_t *phy = esp_eth_phy_new_ip101(&phy_config);
+#elif CONFIG_PHY_RTL8201
+    esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
+#elif CONFIG_PHY_LAN8720
+    esp_eth_phy_t *phy = esp_eth_phy_new_lan8720(&phy_config);
+#elif CONFIG_PHY_DP83848
+    esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
 #endif
 
-    ret = esp_eth_init(&config);
+    esp_eth_config_t config = ETH_DEFAULT_CONFIG(mac, phy);
+    esp_eth_handle_t eth_handle = NULL;
+    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &eth_handle));
 
-    if(ret == ESP_OK) {
-        esp_eth_enable();
-        return true;
-    }
-
-    return false;
+    return true;
 }
 #endif
 
