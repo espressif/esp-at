61d32f8ef0f01836d7a10657f1f8ba1bb7579493
Chen Wu <chenwu@espressif.com>
2022-07-26 21:27:29 +0800

    fix(ESPAT-1216): Fixed that GPIO32/GPIO33 cannot work
    Reason:
    esp-at enabled BLE light sleep by default, which requires an external 32 KHz oscillator,
    so that XTAL_32K_P(32K_XP) and XTAL_32K_N(32K_XN) can not used for common GPIOs
    even if there is no a physically external 32 KHz oscillator.
    More details are in RTC clock part of <esp_hardware_design_guidelines_en.pdf>.
    
    Fix:
    In order to make XTAL_32K_P(32K_XP) and XTAL_32K_N(32K_XN) can be used for common GPIOs,
    esp-at will disable external rtc clock if there is no a physically external 32 KHz oscillator.
    
    - Closes https://github.com/espressif/esp-at/issues/653
    - Closes https://jira.espressif.com:8443/browse/ESPAT-1216

--- main/app_main.c
+++ main/app_main.c
@@ -67,6 +67,20 @@ static void at_disable_rtc_wdt(void)
 }
 #endif
 
+#if defined(CONFIG_ESP32_RTC_CLK_SRC_EXT_CRYS) || defined(CONFIG_RTC_CLK_SRC_EXT_CRYS)
+#include "soc/rtc.h"
+
+static void at_disable_rtc_clk_32k_if_no_ext_crys(void)
+{
+    if (rtc_clk_slow_freq_get() != RTC_SLOW_FREQ_32K_XTAL) {
+        rtc_clk_32k_enable(false);
+        printf("no external 32k oscillator, disable it now.\r\n");
+    } else {
+        printf("external 32k oscillator already started.\r\n");
+    }
+}
+#endif
+
 #ifdef CONFIG_AT_WIFI_COMMAND_SUPPORT
 static void at_wifi_init(void)
 {
@@ -106,6 +120,21 @@ void app_main(void)
      * Here, disable RWDT to avoid the second restart (RTCWDT_RTC_RESET).
      */
     at_disable_rtc_wdt();
+#endif
+
+#if defined(CONFIG_ESP32_RTC_CLK_SRC_EXT_CRYS) || defined(CONFIG_RTC_CLK_SRC_EXT_CRYS)
+    /**
+     * A workaround for external 32 KHz oscillator.
+     *
+     * esp-at enabled BLE light sleep by default, which requires an external 32 KHz oscillator,
+     * so that XTAL_32K_P(32K_XP) and XTAL_32K_N(32K_XN) can not used for common GPIOs
+     * even if there is no a physically external 32 KHz oscillator.
+     * More details are in RTC clock part of <esp_hardware_design_guidelines_en.pdf>.
+     *
+     * In order to make XTAL_32K_P(32K_XP) and XTAL_32K_N(32K_XN) can be used for common GPIOs,
+     * esp-at will disable external rtc clock if there is no a physically external 32 KHz oscillator.
+     */
+    at_disable_rtc_clk_32k_if_no_ext_crys();
 #endif
 
     ESP_ERROR_CHECK(nvs_flash_init());
