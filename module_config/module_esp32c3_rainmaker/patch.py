9e3315f108abd8629ddcc54d0f7e7597a23605ba
Chen Wu <chenwu@espressif.com>
2023-05-06 15:35:45 +0800

    fix(ESPAT-1635): Fixed the default end ip address of dhcp server changed from 192.168.4.101 to 192.168.4.100

--- module_config/module_esp32c3_rainmaker/patch.py
+++ module_config/module_esp32c3_rainmaker/patch.py
@@ -40,7 +40,8 @@ def main():
     patch_list = {
         'rmaker-esp_rainmaker.patch': os.path.join(cur_abs_dir, 'components', 'rainmaker', 'esp-rainmaker', 'components', 'esp_rainmaker'),
         'rmaker-rmaker_common.patch': os.path.join(cur_abs_dir, 'components', 'rainmaker', 'esp-rainmaker', 'components', 'rmaker_common'),
-        'softap_pmk.patch': os.path.join(cur_abs_dir, 'esp-idf')
+        'softap_pmk.patch': os.path.join(cur_abs_dir, 'esp-idf'),
+        'dhcps.patch': os.path.join(cur_abs_dir, 'esp-idf')
     }
 
     for name, path in patch_list.items():
